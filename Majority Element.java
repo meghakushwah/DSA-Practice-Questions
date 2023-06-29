6c253994a49fcf933852adf15775076ca5ece44e
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2023-06-29 15:08:04 +0530

    Update Majority Element.java

--- Majority Element.java
+++ Majority Element.java
@@ -2,16 +2,15 @@ class Solution {
     public int majorityElement(int[] nums) {
          int count = 0;
                 int max = 0;
-				for(int i =0 ; i<nums.length; i++) {
-					if(count == 0) {
-						max = nums[i];
-					}
-					if(nums[i] == max) {
-						count++;
-					} else {
-						count--;
-					}
-				}
+		for(int i =0 ; i<nums.length; i++) {
+		    if(count == 0) {
+		       max = nums[i];
+		    }
+		    if(nums[i] == max) {
+		      count++;
+		    } else {
+		      count--;
+		    }
             return max;
     }
 }
