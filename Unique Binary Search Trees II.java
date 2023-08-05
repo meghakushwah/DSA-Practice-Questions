29943af4637042f927acbdaaa2ec4b9ace81a8ea
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2023-08-05 20:15:11 +0530

    Create Unique Binary Search Trees II.java

---
+++ Unique Binary Search Trees II.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        return upvote(1,n);
+    }
+    private List<TreeNode> upvote(int s, int e){
+        List<TreeNode> ans = new ArrayList<>();
+        if(s>e){
+            ans.add(null);
+            return ans;
+        }
+        for(int i = s;i <= e;i++){
+            List<TreeNode> left = upvote(s, i-1);
+            List<TreeNode> right = upvote(i+1, e);
+
+            for(TreeNode l : left){
+                for(TreeNode r : right){
+                    TreeNode res = new TreeNode(i,l,r);
+                    ans.add(res);
+                }
+            }
+        }
+        return ans;
+    }
+}
