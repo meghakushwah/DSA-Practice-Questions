da9266719f5c0dd2310a2be55d712d9ca1327054
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2023-08-12 14:43:57 +0530

    Create Binary Tree Inorder Traversal.java

---
+++ Binary Tree Inorder Traversal.java
@@ -0,0 +1,12 @@
+class Solution {
+    private List<Integer> ans = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root==null) {
+            return ans;
+        }
+        inorderTraversal(root.left);
+        ans.add(root.val);
+        inorderTraversal(root.right);
+        return ans;
+    }
+}
