39076d5460cc0d1fd7dfe293a29d42fbad845bae
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2023-08-16 18:03:23 +0530

    Create Validate Binary Search Tree.java

---
+++ Validate Binary Search Tree.java
@@ -0,0 +1,15 @@
+class Solution {
+    
+    public boolean isValidBST(TreeNode root) {
+        return solve(root,Long.MIN_VALUE,Long.MAX_VALUE);    
+    }
+
+    public static boolean solve(TreeNode root,long min,long max){
+        if(root==null) return true;
+
+        if(root.val>=max || root.val<=min) return false;
+
+        return solve(root.left,min,root.val) && solve(root.right,root.val,max);
+    }
+
+}
