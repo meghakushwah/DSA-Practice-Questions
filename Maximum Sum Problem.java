55505d9b3c62245d7d7ac8dc1fcff741dff5cf53
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2024-02-24 13:08:46 +0530

    Create Maximum Sum Problem.java

---
+++ Maximum Sum Problem.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    public int maxSum(int n) 
+    { 
+        //code here.
+        int sum =0;
+        if(n==0) return 0;
+        
+        int s1 = maxSum(n/2);
+        int s2 = maxSum(n/3);
+        int s3 = maxSum(n/4);
+        
+        sum = Math.max(n,s1+s2+s3);
+        
+        return sum;
+        
+    } 
+}
