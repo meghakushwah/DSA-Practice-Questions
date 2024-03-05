0eacb8bbfe7f610a0a592f3abdfcdcba6c5adfe6
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2024-03-05 23:48:37 +0530

    Update Maximum Index.java

--- Maximum Index.java
+++ Maximum Index.java
@@ -1,28 +1,31 @@
 class Solution{
-    
     static int maxIndexDiff(int arr[], int n) { 
         
-        // Your code here
-        int i=0;
-        int j=n-1;
-        int max=0;
-        int maxIndex=1;
-        while(i<j && j<n && (j-i)>=max)
-        {
-            while(arr[i]>arr[j] && maxIndex<j) j--;
-            if(j!=i)
-            {
-                maxIndex=Math.max(j,maxIndex);
-                max=Math.max(max,j-i);
-            }
-            
-            j=n-1;
-            i++;
-                
-            
-        }
-        return max;
+        int minVal[] = new int[n];
+        int maxVal[] = new int[n];
         
+        //for min : minVal[i] = min(arr[i],arr[i-1]);
+        minVal[0] = arr[0];
+        for(int i=1;i<n;i++){
+            minVal[i] = Math.min(arr[i],minVal[i-1]);
+        }
+        
+        //for max : maxVal[i] = max(arr[i],maxVal[i+1]);
+        maxVal[n-1] = arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            maxVal[i] = Math.max(arr[i],maxVal[i+1]);
+        }
+        int i = 0;
+        int j = 0;
+        int ans = Integer.MIN_VALUE;
+        while(i<n && j<n){
+            if(minVal[i]<=maxVal[j]){// Given : a[i] < a[j] and i < j
+                ans = Math.max(ans,j-i);
+                j++;
+            }else{
+                i++;
+            }
+        }
+        return ans;
     }
 }
-
