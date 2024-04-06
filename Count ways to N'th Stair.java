c01537b27fa4a40d1595f90ba46b932f083daf4d
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2024-04-06 18:43:56 +0530

    Create Count ways to N'th Stair.java

---
+++ Count ways to N'th Stair.java
@@ -0,0 +1,14 @@
+class Solution {
+    Long countWays(int n) {
+        // your code here
+        if(n<2) return (long)n;
+        long[] dp = new long[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<=n;i++){
+            dp[i] = Math.max(dp[i-2]+1, dp[i-1]);
+        }
+        return dp[n];
+    }
+}
