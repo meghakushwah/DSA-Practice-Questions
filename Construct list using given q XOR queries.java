f8783a9610e4fc8c1e2895bae1895da89a2661fd
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2024-06-02 12:09:56 +0530

    Create Construct list using given q XOR queries.java

---
+++ Construct list using given q XOR queries.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static ArrayList<Integer> constructList(int q, int[][] queries) {
+        ArrayList<Integer> list  = new ArrayList<>();
+        
+        int xortillnow = 0;
+        
+        for(int i = q -1 ; i>=0; i--) {
+            if(queries[i][0] == 1) {
+                xortillnow ^=queries[i][1];
+            }else {
+                list.add(queries[i][1] ^xortillnow);
+            }
+        }
+        
+        list.add(xortillnow);
+        Collections.sort(list);
+        return list;
+    }
+}
