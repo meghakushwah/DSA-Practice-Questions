6558cfbf5a2526bd40fb17400d6f9885843a5756
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2024-06-29 11:47:52 +0530

    Create Identical Linked Lists.java

---
+++ Identical Linked Lists.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean are Identical(Node head1, Node head2) {
+      while (head1 != null && head2 != null) {
+  
+            if (head1.data != head2.data) {
+                return false;
+            }
+      
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+     
+        return (head1 == null && head2 == null);
+    }
+}
