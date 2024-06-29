4e732f156840213c4749ed733eddb8c1f2840d63
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2024-06-29 11:56:53 +0530

    Update Identical Linked Lists.java

--- Identical Linked Lists.java
+++ Identical Linked Lists.java
@@ -1,15 +1,20 @@
 class Solution {
-    public boolean are Identical(Node head1, Node head2) {
-      while (head1 != null && head2 != null) {
-  
-            if (head1.data != head2.data) {
-                return false;
+    public boolean areIdentical(Node head1, Node head2) {
+        // write your code here
+        Node temp1=head1;
+        Node temp2=head2;
+
+        while(temp1!=null && temp2!=null){
+            if(temp1.data!=temp2.data){
+               return false;
             }
-      
-            head1 = head1.next;
-            head2 = head2.next;
+            
+             temp1=temp1.next;
+             temp2=temp2.next;
+            
+            if((temp1==null && temp2!=null) || (temp1!=null && temp2==null))return false;
         }
-     
-        return (head1 == null && head2 == null);
+        
+        return true;
     }
 }
