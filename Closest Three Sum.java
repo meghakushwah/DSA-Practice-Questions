70a48329ba701c6b95bd7d20c0c4f38920e761c5
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2024-07-09 15:31:24 +0530

    Create Closest Three Sum.java

---
+++ Closest Three Sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    static int threeSumClosest(int[] array, int target) {
+        
+        Arrays.sort(array);
+        int min = Integer.MAX_VALUE;
+        int correct = 0;
+        for(int i = 0; i<array.length-1; i++){
+            int j = i+1;
+            int k = array.length-1;
+            
+            while(j<k){
+                int sum = array[i]+ array[j] + array[k];
+                int ans = Math.abs(sum - target);
+                if(sum == target){
+                    return sum;
+                }
+                if(sum<target){
+                    if(min>ans){
+                        min = ans;
+                        correct = sum;
+                    }
+                }
+                if(sum > target){
+                    if(min>=ans){
+                        min = ans;
+                        correct = sum;
+                    }
+                }
+                
+                if(sum<target){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+            }
+        }
+        return correct;
+    }
+}
