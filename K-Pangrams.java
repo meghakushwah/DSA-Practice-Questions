50d439a4beeee8bafdfb20e1783b9b87f494348a
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2024-07-26 22:14:24 +0530

    Create K-Pangrams.java

---
+++ K-Pangrams.java
@@ -0,0 +1,22 @@
+class Solution {
+    boolean kPangram(String str, int k) {
+        // code here
+        int [] arr = new int[26];
+        int count = 0;
+        int op = 0;
+        
+        for(int i=0; i < str.length(); i++) {
+            if(str.charAt(i) != ' ') {
+                arr[str.charAt(i) - 'a'] ++;
+                count++;
+            }
+        }
+        for(int i=0; i < 26; i++) {
+            if(arr[i] == 0) {
+                op++;
+            }
+        }
+        return(op <= k) && (count >= 26);
+    }
+}
+
