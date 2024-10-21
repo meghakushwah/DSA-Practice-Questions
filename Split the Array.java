1f17ee6d76e1a19864dbbfd6d9d42811a48f4774
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2024-10-21 21:39:59 +0530

    Create Split the Array.java

---
+++ Split the Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int countgroup(int arr[]) {
+        // Complete the function
+         int a = 0;
+        for (int i : arr) {
+            a ^= i;  
+        }
+        if (a == 0) {
+            int n = arr.length;
+            return (int)Math.pow(2, n - 1)-1;
+        }
+        return 0;
+    }
+}
