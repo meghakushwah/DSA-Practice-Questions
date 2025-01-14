8bf31df5f537b84dccd99cd4e5cc857b2bf4ea4e
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2025-01-14 20:45:16 +0530

    Create Equilibrium Point.java

---
+++ Equilibrium Point.java
@@ -0,0 +1,23 @@
+class Solution {
+    // Function to find equilibrium point in the array.
+    public static int findEquilibrium(int arr[]) {
+        // code here
+        int n=arr.length;
+        int prev = 0;
+        int sum = 0;
+       
+        for(int i=0; i<n; i++) {
+            sum += arr[i];
+        }
+       
+        for(int i = 0; i < n; i++) {
+            sum -= arr[i];
+           
+           if(prev == sum) {
+               return i;
+           }
+           prev += arr[i];
+        }
+        return -1;
+    }
+}
