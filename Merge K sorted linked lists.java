76d1e93272c83fb445f40cb276b21809b32ce3d5
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2025-02-19 21:10:08 +0530

    Update Merge K sorted linked lists.java

--- Merge K sorted linked lists.java
+++ Merge K sorted linked lists.java
@@ -1,36 +1,26 @@
 class Solution {
     // Function to merge K sorted linked list.
     Node mergeKLists(List<Node> arr) {
-        int i = 0;
-        int last = arr.size() - 1;
-        int j;
-        while(last != 0){
-            i = 0;
-            j = last;
-            while(i < j){
-                arr.set(i, merge(arr.get(i), arr.get(j)));
-                i++;
-                j--;
-                if(i >= j){
-                    last = j;
-                }
+        // Add your code here.
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b)->a.data-b.data);
+        for(Node n: arr){
+            pq.add(n);
+        }
+        Node newHead=null, tail=null;
+        while(!pq.isEmpty()){
+            Node tmp = pq.poll();
+            if(newHead == null){
+                newHead = tmp;
+                tail=tmp;
+            }
+            else {
+                tail.next = tmp;
+                tail = tmp;
+            }
+            if(tmp.next!=null){
+                pq.add(tmp.next);
             }
         }
-        return arr.get(0);
-    }
-    
-    Node merge(Node a, Node b){
-        if(a == null) return b;
-        if(b == null) return a;
-        Node result;
-        if(a.data <= b.data){
-            result = a;
-            result.next = merge(a.next, b);
-        }
-        else{
-            result = b;
-            result.next = merge(a, b.next);
-        }
-        return result;
+        return newHead;
     }
 }
